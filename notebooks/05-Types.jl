3844c0ff4bbc7ffa10743efc168ca63a53e52f45
Abhro <5664668+abhro@users.noreply.github.com>
2025-11-11 09:35:26 -0500

    Create 05-Types.jl

---
+++ notebooks/05-Types.jl
@@ -0,0 +1,39 @@
+### A Pluto.jl notebook ###
+# v0.20.20
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 189ec852-bf0b-11f0-a069-2def1b4d4791
+using DrWatson
+
+# ╔═╡ 169ccf8b-5f34-4e1c-9596-a50c6153b9f1
+@quickactivate "AAS247Julia"
+
+# ╔═╡ d1a478f0-5282-4a8c-b44f-d3297748e9c3
+using PlutoUI, HypertextLiteral
+
+# ╔═╡ 520278e3-1521-438a-bdbd-088956a4cc42
+md"""
+# Types
+"""
+
+# ╔═╡ e3d0f321-ded7-4355-abdb-d58fbc008cd1
+md"""
+# Notebook setup
+"""
+
+# ╔═╡ 488e18dd-a795-457f-a78c-36090a63d1a1
+notebookName = "Types";
+
+# ╔═╡ 56d5d99d-88a6-4e35-92da-3a7a8ba3a0a6
+TableOfContents(title = notebookName)
+
+# ╔═╡ Cell order:
+# ╟─520278e3-1521-438a-bdbd-088956a4cc42
+# ╟─e3d0f321-ded7-4355-abdb-d58fbc008cd1
+# ╠═189ec852-bf0b-11f0-a069-2def1b4d4791
+# ╠═169ccf8b-5f34-4e1c-9596-a50c6153b9f1
+# ╠═d1a478f0-5282-4a8c-b44f-d3297748e9c3
+# ╟─488e18dd-a795-457f-a78c-36090a63d1a1
+# ╠═56d5d99d-88a6-4e35-92da-3a7a8ba3a0a6
